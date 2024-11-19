31facafb6fc955c0dc2a0c169083fb15c58121bf
minjun011026 <jun011026@gmail.com>
2024-11-19 21:18:19 +0900

    feat: ui

--- app/src/main/java/com/example/ssul/SelectDepartmentFragment.kt
+++ app/src/main/java/com/example/ssul/SelectDepartmentFragment.kt
@@ -17,14 +17,43 @@ class SelectDepartmentFragment : Fragment() {
     private val viewModel: DepartmentRegisterViewModel by activityViewModels()
 
     private val departmentMap = mapOf(
-        "IT대학" to listOf("컴퓨터학부", "소프트웨어학부", "글로벌미디어학부", "전자정보공학부(전자공학전공)", "전자정보공학부(IT융합전공)", "미디어경영학과"," 정보보호학과"),
-        "인문대학" to listOf("국어국문학과", "영어영문학과", "사학과", "기독교학과", "독어독문학과", "불어불문학과", "중어중문학과", "일어일문학과", "철학과", "예술창작학부", "스포츠학부"),
+        "IT대학" to listOf(
+            "컴퓨터학부",
+            "소프트웨어학부",
+            "글로벌미디어학부",
+            "전자정보공학부(전자공학전공)",
+            "전자정보공학부(IT융합전공)",
+            "미디어경영학과",
+            "정보보호학과"
+        ),
+        "인문대학" to listOf(
+            "국어국문학과",
+            "영어영문학과",
+            "사학과",
+            "기독교학과",
+            "독어독문학과",
+            "불어불문학과",
+            "중어중문학과",
+            "일어일문학과",
+            "철학과",
+            "예술창작학부",
+            "스포츠학부"
+        ),
         "법과대학" to listOf("법학과", "국제법학과"),
         "공과대학" to listOf("기계공학부", "전기공학부", "건축학부", "화학공학과", "산업정보시스템공학과", "신소재공학과"),
         "자연과학대학" to listOf("수학과", "물리학과", "화학과", "정보통계보험수리학과", "의생명시스템학부"),
         "사회과학대학" to listOf("정치외교학과", "사회복지학부", "행정학부", "정보사회학과", "언론홍보학과", "평생교육학과"),
         "경제통상대학" to listOf("경제학과", "국제무역학과", "금융경제학과", "통상산업학과", "글로벌통상학과"),
-        "경영대학" to listOf("경영학부", "회계학과", "벤처경영학과", "복지경영학과", "회계세무학과", "혁신경영학과", "금융학부", "벤처중소기업학과"),
+        "경영대학" to listOf(
+            "경영학부",
+            "회계학과",
+            "벤처경영학과",
+            "복지경영학과",
+            "회계세무학과",
+            "혁신경영학과",
+            "금융학부",
+            "벤처중소기업학과"
+        ),
         "융합특성화자유전공학부" to listOf("융합특성화자유전공학부"),
         "차세대반도체학과" to listOf("차세대반도체학과"),
         "베어드교양학과" to listOf("기독교교육", "의사소통교육", "영어교육", "IT정보교육", "소양교육", "베어드교양대학 산하 기관")
@@ -45,14 +74,20 @@ class SelectDepartmentFragment : Fragment() {
             // 이전 프래그먼트에서 전달된 단과대 가져오기
             val selectedCollege = viewModel.selectedCollege.value ?: "학과를 선택하세요"
 
-            val departments = listOf(selectedCollege) + (departmentMap[selectedCollege] ?: listOf("학과 없음"))
+            val departments =
+                listOf(selectedCollege) + (departmentMap[selectedCollege] ?: listOf("학과 없음"))
 
-            val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, departments)
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            val adapter = ArrayAdapter(requireContext(), R.layout.spinner_item, departments)
+            adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
             it.adapter = adapter
 
             it.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
                     // 기본 텍스트가 아닌 항목이 선택된 경우에만 처리
                     if (position != 0) {
                         val selectedDepartment = departments[position]
